96328a65fa0cf0764620e118b55efbd95c052c6d
Mahmoud_Walid <modywmbadr@gmail.com>
2025-05-18 19:58:41 +0300

    feat(ui): add EmojiPickerButton component for emoji selection
    EmojiPickerButton component, which allows users to select emojis and insert them into an input field. The component includes various configurations such as theme, language, and position, and uses hooks for state management and emoji data fetching.

---
+++ src/ui/EmojiPickerButton.tsx
@@ -0,0 +1,134 @@
+import EmojiInputPicker from "./EmojiInputPicker";
+import { type RefObject } from "react";
+// import emojiData from "../data/categories.with.modifiers.json";
+import "./index.css";
+import {
+  defaultBadEmojisList,
+  defaultThemeConfig,
+  getPickerPositionStyle,
+  sizeConfig,
+  type PickerPosition,
+} from "../utils/constants";
+import useCountryFlagEmojiPolyfill from "../hooks/useCountryFlagEmojiPolyfill";
+import usePickerSize from "../hooks/usePickerSize";
+import Button from "./Button";
+import useEmojiData from "../hooks/useEmojiData";
+import useInputPlaceholderAndDir from "../hooks/useInputPlaceholderAndDir";
+import useEmojiPickerButtonState from "../hooks/useEmojiPickerButtonState";
+
+interface IProps {
+  textInputRef: RefObject<HTMLInputElement | HTMLTextAreaElement | null>;
+  classNames?: EmojiPickerClassNames;
+  config?: EmojiPickerConfig;
+  initialCategory?: string;
+  onEmojiSelect?: (emoji: string) => void;
+  styles?: EmojiPickerStyles;
+  position?: PickerPosition;
+  theme?: "dark" | "light";
+  themeConfig?: ThemeConfig;
+  lang?: "ar" | "en";
+  enableRecentEmojis?: boolean;
+  sizes?: EmojiPickerSizes;
+  size?: "sm" | "md" | "lg";
+  enableTabsTitleEmoji?: boolean;
+  preventBadEmojis?: boolean;
+  badEmojiList?: string[];
+}
+
+const EmojiPickerButton = ({
+  textInputRef,
+  classNames,
+  config,
+  initialCategory,
+  onEmojiSelect,
+  styles,
+  position = "top-left",
+  theme = "light",
+  themeConfig = defaultThemeConfig,
+  lang = "en",
+  enableRecentEmojis = true,
+  sizes = {},
+  size = "md",
+  enableTabsTitleEmoji = true,
+  preventBadEmojis = true,
+  badEmojiList = defaultBadEmojisList,
+}: IProps) => {
+  const {
+    currentEmoji,
+    pickerRef,
+    setCurrentEmoji,
+    setShowEmojiPicker,
+    showEmojiPicker,
+  } = useEmojiPickerButtonState();
+
+  useCountryFlagEmojiPolyfill();
+
+  const { pickerSize } = usePickerSize({ pickerRef, showEmojiPicker });
+  const mergedConfig: EmojiPickerConfig = {
+    ...config,
+    themeConfig,
+    enableRecentEmojis,
+    ...sizes,
+    ...sizeConfig[size],
+  };
+
+  const { emojiData, isLoading } = useEmojiData({ lang, showEmojiPicker });
+
+  console.log("isLoading", isLoading);
+
+  useInputPlaceholderAndDir({ lang, textInputRef });
+
+  return (
+    <div
+      className={`emoji-picker-container emoji-picker-theme-${theme}`}
+      style={{
+        fontFamily: '"Twemoji Country Flags", "Helvetica", "Comic Sans", serif',
+      }}
+      data-theme={theme}
+    >
+      <Button
+        currentEmoji={currentEmoji}
+        setCurrentEmoji={setCurrentEmoji}
+        setShowEmojiPicker={setShowEmojiPicker}
+        showEmojiPicker={showEmojiPicker}
+        theme={theme}
+      />
+      {showEmojiPicker && (
+        <div
+          className={`emoji-picker-wrapper emoji-picker-position-${position} emoji-picker-theme-${theme}`}
+          style={{
+            position: "absolute",
+            ...getPickerPositionStyle({
+              position,
+              pickerHeight: pickerSize.height,
+              pickerWidth: pickerSize.width,
+            }),
+          }}
+          data-theme={theme}
+        >
+          <EmojiInputPicker
+            isLoading={isLoading}
+            classNames={classNames}
+            config={mergedConfig}
+            initialCategory={initialCategory}
+            onEmojiSelect={onEmojiSelect}
+            styles={styles}
+            inputRef={
+              textInputRef as RefObject<HTMLInputElement | HTMLTextAreaElement>
+            }
+            emojiJsonData={emojiData}
+            onClose={() => setShowEmojiPicker(false)}
+            pickerRef={pickerRef}
+            theme={theme}
+            lang={lang}
+            enableTabsTitleEmoji={enableTabsTitleEmoji}
+            preventBadEmojis={preventBadEmojis}
+            badEmojiList={badEmojiList}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EmojiPickerButton;
