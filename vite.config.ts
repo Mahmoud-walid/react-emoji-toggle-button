aa091d010e72ab2473e1742f0db6236a24ce904d
Mahmoud_Walid <modywmbadr@gmail.com>
2025-05-18 18:11:38 +0300

    chore: Init ignore files and update package.json and update vite.config.json for publish the package to npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dtsPlugin from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dtsPlugin({ tsconfigPath: "./tsconfig.build.json" })],
+  build: {
+    outDir: "build",
+    lib: {
+      entry: "./src/index.tsx",
+      name: "react-emoji-input-picker",
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
